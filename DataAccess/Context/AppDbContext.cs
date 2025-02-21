00eac928f481608bbabf6b566253ef8d4267ae84
Erdem <erdemsincer123@icloud.com>
2025-02-21 20:12:07 +0300

    Add project files.

---
+++ DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context;
+
+public sealed class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=DESKTOP-SORC49E\\MSSQLSERVER02;Initial Catalog=TipsForNetDevelopersDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    }
+
+     public DbSet<Product>  Products { get; set; }
+}
