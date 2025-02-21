32539a16b6c6f49e772b949e7afa2fab8121b970
Erdem <erdemsincer123@icloud.com>
2025-02-21 23:26:52 +0300

    Cancellection işelmi

---
+++ CancellationTokenWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CancellationTokenWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+        {
+            AppDbContext context = new();
+            IList<Product> products=await context.Products.ToListAsync(cancellationToken);//istek iptal edildiğinde işlemler cevap verme isteğini durdurur.
+            return Ok(products);
+        }
+    }
+}
