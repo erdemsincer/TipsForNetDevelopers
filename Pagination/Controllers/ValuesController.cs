248c35b6bce9ddf8318ad9a904fe7affe4076185
Erdem <erdemsincer123@icloud.com>
2025-02-21 23:04:16 +0300

    sayfalama işlemi

---
+++ Pagination/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pagination.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        AppDbContext context = new();
+        [HttpGet]
+
+        public IActionResult Get(int pageNumber,int pageSize)
+        {
+            PaginationDto<Product> result = new();
+            IList<Product> products = 
+                context.Products
+                .Skip((pageNumber-1)*pageSize)
+                .Take(pageSize)
+                .ToList();
+            int count=context.Products.Count();
+            result.Datas= products;
+            result.PageNumber=pageNumber;
+            result.PageSize = pageSize;
+            result.IsFirstPage = pageNumber == 1 ? true : false;
+            result.TotalPageCount = (int)Math.Ceiling(count / (double)pageSize);
+            result.IsLastPage = pageNumber == result.TotalPageCount ? true : false;
+
+            return Ok(result);
+        } 
+        
+    }
+}
