575584dfbc89e15e16ea6285abcf981e88a8b843
Erdem <erdemsincer123@icloud.com>
2025-02-21 20:34:13 +0300

    Benchmark

---
+++ Benchmark.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmark.ConsoleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        BenchmarkRunner.Run<BenchmarkService>();
+    }
+}
